de5902b80624d861b37026bf9992f222d669f250
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-16 23:32:28 +0900

    init commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(handler));
+
+    // run it with hyper on localhost:8080
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, axum!!"
+}
